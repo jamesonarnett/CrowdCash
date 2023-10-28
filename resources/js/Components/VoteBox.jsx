945120c03e6582d4ab795a96bb2eedd7c9f6f3bb
jamesonarnett <jamesonarnett@yahoo.com>
2023-10-28 16:54:51 -0500

    https?

--- resources/js/Components/VoteBox.jsx
+++ resources/js/Components/VoteBox.jsx
@@ -1,18 +1,21 @@
 import React from "react";
 import { useEffect, useState } from "react";
-import { BiSolidUpvote, BiSolidDownvote } from "react-icons/bi";
+import { BiSolidUpvote } from "react-icons/bi";
+import ToolTip from "./inputs/ToolTip";
 
 const VoteBox = ({ post }) => {
     const [upvotes, setUpvotes] = useState(0);
 
+    console.log(post);
     return (
         <div className="flex items-center">
-            <button className="hover:bg-primary text-white font-bold py-2 px-4 rounded">
-                <BiSolidUpvote fill="#000000" />
-            </button>
-
-            <button className="hover:bg-quaternary text-white font-bold py-2 px-4 rounded">
-                <BiSolidDownvote fill="#000000" />
+            <button className="text-white font-bold flex p-2 rounded">
+                <ToolTip text="Vote!">
+                    <BiSolidUpvote
+                        fill="#000000"
+                        className="hover:fill-primary text-xl"
+                    />
+                </ToolTip>
             </button>
 
             <div className="">{upvotes}</div>
--- resources/js/Components/nav/Navbar.jsx
+++ resources/js/Components/nav/Navbar.jsx
@@ -48,7 +48,7 @@ const Navbar = ({ user }) => {
 
                                             <svg
                                                 className="ml-2 -mr-0.5 h-4 w-4"
-                                                xmlns="http://www.w3.org/2000/svg"
+                                                xmlns="https://www.w3.org/2000/svg"
                                                 viewBox="0 0 20 20"
                                                 fill="currentColor"
                                             >
